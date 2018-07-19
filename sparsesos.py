5940f1015a23aa6b0a260b1729e50434d8fef576
haokun li <ker@protonmail.ch>
2018-07-19 14:22:34 +0800

    bug fix

--- sparsesos.py
+++ sparsesos.py
@@ -1,6 +1,7 @@
 import sys
 import os
-from sage.all import *
+from sage.all import QQ,PolynomialRing
+from sage.rings.polynomial.polydict import ETuple
 def strtoPolynomial(s):
     def is_good(x):
         return (x<="z" and x>="a") or (x<="Z" and x>="A") or (x<="9" and x>="0") or x in "()+-*^_"
@@ -58,85 +59,102 @@ def com(R):
     for k in range(n):
         for i in range(n):
             for j in range(n):
-                if not R[i,j] and R[i,k] and R[j,k] :
-                    R[i,j]=1
-                    R[j,i]=1
+                if not R[i][j] and R[i][k] and R[j][k] :
+                    R[i][j]=1
+                    R[j][i]=1
     L=[];k=[0]*n;
-    for i in range(n)
+    for i in range(n):
         if k[i]==0:
             L.append([])
             for j in range(n):
-                if R[i,j]:
+                if R[i][j]:
                     k[j]=1
                     L[-1].append(j)
     return L
 def is_sparsesos(f,f1="sparsesos.dat",f2="sparsesos.result"):
-    def tuple_add(l1,l2):
-        n=min(len(l1),len(l2))
-        l=[0]*n;
-        for i in range(n):
-            l=l1[i]+l2[i]
-        return tuple(l)
     def tuple_even(l):
         for i in l:
             if i % 2!=0:
                 return False
         return True
+    print(f)
     dct=f.dict()
     P1=f.newton_polytope()
-    P_points=P1.integral_points()
-    points=((1/2)*P1).integral_points()
+    #P_points=P1.integral_points()
+    P2=(QQ(1)/2)*P1
+    points=P2.integral_points()
+    #print(dct,points,P_points,P2,P1)
     n=len(points)
     R=[];
     for i in range(n):
         R.append([0]*n)
         for j in range(n):
-            l=tuple_add(points[i],points[j])
+            l=ETuple(tuple(points[i]+points[j]))
+            #print(l,dct)
+            #l in dct
             if tuple_even(l) or l in dct:
-                R[i,j]=1
+                R[i][j]=1
     L=com(R)
-    S=[]
-    ld=list(map(len,L))
-    fout=open(f1,"w");
-    fout.write("%d=mDIM\n %d=nBLOCK\n"% (len(P_points),len(L)))
-    fout.write(" ".join(map(str,ld))+"= bLOCKsTRUCT\n")
-    c=[]
-    for i in P_points:
-        if i in dct:
-            c.append(dct[i])
-        else:
-            c.append(0)
-    fout.write("{"+",".join(map(str,c))+"}\n")
+    S=set()
     co=[]
     for i in range(len(L)):
         co1=[]
         for j in range(len(L[i])):
             co1.append([])
             for k in range(len(L[i])):
-                co1[-1].append(tuple_add(points[L[i][j]],points[L[i][k]]))
+                lk=ETuple(tuple(points[L[i][j]]+points[L[i][k]]))
+                co1[-1].append(lk)
+                S.add(lk)
         co.append(co1)
+    P_points=list(S)
+    fout=open(f1,"w");
+    fout.write("%d=mDIM\n%d=nBLOCK\n"% (len(P_points),len(L)))
+    fout.write(" ".join(map(str,map(len,L)))+"= bLOCKsTRUCT\n")
+    c=[]
+    for i in P_points:
+        #i1=ETuple(tuple(i))
+        if i in dct:
+            c.append(dct[i])
+        else:
+            c.append(0)
+    fout.write("{"+",".join(map(str,c))+"}\n")
+    for j in range(len(L)):
+        fout.write("{")
+        co1=["0"]*len(L[j])
+        for k1 in range(len(L[j])):
+            if k1==0:
+                fout.write("{")
+            else:
+                fout.write(",{")
+
+            fout.write(",".join(co1))
+            fout.write("}")
+        fout.write("}\n")
+    fout.write("}\n")
     for i in P_points:
         fout.write("{")
         for j in range(len(L)):
-                fout.write("{")
-                co1=[0]*len(L[j])
-                for k1 in range(len(L[j])):
-                    if k1==0:
-                        fout.write("{")
+            fout.write("{")
+            co1=[0]*len(L[j])
+            for k1 in range(len(L[j])):
+                if k1==0:
+                    fout.write("{")
+                else:
+                    fout.write(",{")
+
+                for k2 in range(len(L[j])):
+                    if co[j][k1][k2]==i:
+                        co1[k2]="1"
                     else:
-                        fout.write(",{")
+                        co1[k2]="0"
+                fout.write(",".join(co1))
+                fout.write("}")
 
-                    for k2 in range(len(L[j])):
-                        if co[j][k1][k2]==i:
-                            co1[k2]="1"
-                        else:
-                            co1[k2]="0"
-                    fout.write(",".join(co1))
-                    fout.write("}")
-
-                fout.write("}\n")
+            fout.write("}\n")
 
         fout.write("}\n")
+
+
     fout.close()
     os.system("sdpa %s %s" % (f1,f2))
 
