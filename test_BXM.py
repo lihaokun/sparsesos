872c95696bf147e7a4a16f4c5e0e82e2b60e263c
haokun li <ker@protonmail.ch>
2018-07-20 22:23:00 +0800

    一些更新

--- test_BXM.py
+++ test_BXM.py
@@ -5,7 +5,7 @@ def BXM(m):
     y=PolynomialRing(QQ,3*m+2,"x").gens();
     x=[]
     n=3*m+2
-    f1=0;f2=0
+    f1=0;f2=0;f3=0;
     for i in range(n):
         x.append(y[i]**2)
     for i in range(3*(m+1)):
@@ -14,7 +14,7 @@ def BXM(m):
         f1+=x[i]
         for j in range(m+1):
             f2+=x[i]*x[i+3*j+1]
-    f=(f1**2-2*f2)*(f1);
+    f=(f1**2-2*f2)*f1;
     print(len(f.dict()))
     return(f)
 
