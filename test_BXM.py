92e3f7973dafb7ca9417a630f6eabbbe45792f75
haokun li <ker@protonmail.ch>
2018-07-20 18:27:12 +0800

    add test B(X,m)

--- test_BXM.py
+++ test_BXM.py
@@ -1,6 +1,24 @@
 from sage.all import QQ,PolynomialRing
-import is_sparsesos
+import sparsesos
 
 def BXM(m):
-    x=PolynomialRing(QQ,3*m+2,"x");
-    
+    y=PolynomialRing(QQ,3*m+2,"x").gens();
+    x=[]
+    n=3*m+2
+    f1=0;f2=0
+    for i in range(n):
+        x.append(y[i]**2)
+    for i in range(3*(m+1)):
+        x.append(x[i])
+    for i in range(n):
+        f1+=x[i]
+        for j in range(m+1):
+            f2+=x[i]*x[i+3*j+1]
+    f=(f1**2-2*f2)*(f1);
+    print(len(f.dict()))
+    return(f)
+
+if __name__=="__main__":
+    m=int(input("m="))
+    f=BXM(m)
+    sparsesos.is_sparsesos(f)
